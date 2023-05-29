55e66755106c75cb6f040820e491322c438cc9ab
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-05-29 09:47:39 -0500

    validando rutas

--- src/api/components/routes/tasks.router.js
+++ src/api/components/routes/tasks.router.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
 
+const validatorHandler = require('../../../middleware/validator.handler');
+const {
+  createTask,
+  findTaskById,
+  updateTask,
+} = require('../schemas/task.schema');
 const { Tasks } = require('./');
 
 const router = Router();
@@ -13,45 +19,62 @@ router.get('/', async (_, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const task = await Tasks.findOne(id);
-    res.status(200).json(task);
-  } catch (error) {
-    next(error);
+router.get(
+  '/:id',
+  validatorHandler(findTaskById, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const task = await Tasks.findOne(id);
+      res.status(200).json(task);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.post('/', async (req, res, next) => {
-  try {
-    const body = req.body;
-    const newTask = await Tasks.create(body);
-    res.status(201).json(newTask);
-  } catch (error) {
-    next(error);
+router.post(
+  '/',
+  validatorHandler(createTask, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newTask = await Tasks.create(body);
+      res.status(201).json(newTask);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const updateTask = await Tasks.update(id, body);
-    res.status(200).json(updateTask);
-  } catch (error) {
-    next(error);
+router.put(
+  '/:id',
+  validatorHandler(findTaskById, 'params'),
+  validatorHandler(updateTask, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updateTask = await Tasks.update(id, body);
+      res.status(200).json(updateTask);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const removeTask = await Tasks.remove(id);
-    res.status(204).json(removeTask);
-  } catch (error) {
-    next(error);
+router.delete(
+  '/:id',
+  validatorHandler(findTaskById, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const removeTask = await Tasks.remove(id);
+      res.status(204).json(removeTask);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
--- src/api/components/routes/users.router.js
+++ src/api/components/routes/users.router.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
 
+const validatorHandler = require('../../../middleware/validator.handler');
+const {
+  createUser,
+  findUserById,
+  updateUser,
+} = require('../schemas/user.schema');
 const { Users } = require('./');
 
 const router = Router();
@@ -13,45 +19,62 @@ router.get('/', async (_, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await Users.findOne(id);
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
+router.get(
+  '/:id',
+  validatorHandler(findUserById, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await Users.findOne(id);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.post('/', async (req, res, next) => {
-  try {
-    const body = req.body;
-    const newUser = await Users.create(body);
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
+router.post(
+  '/',
+  validatorHandler(createUser, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newUser = await Users.create(body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const updateUser = await Users.update(id, body);
-    res.status(200).json(updateUser);
-  } catch (error) {
-    next(error);
+router.put(
+  '/:id',
+  validatorHandler(findUserById, 'params'),
+  validatorHandler(updateUser, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updateUser = await Users.update(id, body);
+      res.status(200).json(updateUser);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const removeUser = await Users.remove(id);
-    res.status(204).json(removeUser);
-  } catch (error) {
-    next(error);
+router.delete(
+  '/:id',
+  validatorHandler(findUserById, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const removeUser = await Users.remove(id);
+      res.status(204).json(removeUser);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
