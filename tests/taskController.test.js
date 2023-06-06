36e21ad87a9c6c00ed1aceceb3e4e39fee10a8fc
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-06-06 06:49:16 -0500

    test for taskcontroller passing

--- tests/taskController.test.js
+++ tests/taskController.test.js
@@ -7,17 +7,22 @@ describe('TaskController', () => {
     it('should create a new task', async () => {
       // Arrange
       const req = {
-        body: { title: 'Task 1', description: 'This is a task', userId: 1 },
+        body: {
+          title: 'Preparar el desayuno dia1',
+          description: 'pan ketshup pepinillo pan hanburguesa que que!!! dia1',
+          userId: 1,
+        },
       };
-      const res = {};
 
       // Act
-      const result = await taskController.create(req);
+      const result = await taskController.create(req.body);
 
       // Assert
-      // assert.strictEqual(result.status, '201 Created');
-      assert.strictEqual(result.dataValues.title, 'Task 1');
-      assert.strictEqual(result.dataValues.description, 'This is a task');
+      assert.strictEqual(result.dataValues.title, 'Preparar el desayuno dia1');
+      assert.strictEqual(
+        result.dataValues.description,
+        'pan ketshup pepinillo pan hanburguesa que que!!! dia1'
+      );
       assert.strictEqual(result.dataValues.userId, 1);
     });
   });
